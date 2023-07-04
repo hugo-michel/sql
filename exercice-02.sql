b1d4ea3be3bb396f4f0c2b282cef37b977ac0b55
hugo-michel <orayito62@gmail.com>
2023-07-04 16:55:16 +0200

    exo sql 02

--- exercice-02.sql
+++ exercice-02.sql
@@ -25,6 +25,10 @@ WHERE id = 6
 -- Insérez un tag :
 -- - nom : SASS
 -- - description : aucune
+INSERT INTO tag
+(name, description)
+VALUES
+('SASS', 'NULL');
 
 -- Exo 2.2
 -- Insérez une school year :
@@ -32,6 +36,10 @@ WHERE id = 6
 -- - description : La promo de l'année 2023
 -- - date de début : 15 mars 2023
 -- - date de fin : aucune
+INSERT INTO school_year
+(name, description, start_date, end_date)
+VALUES
+('Promo 2023', 'La promo de l''année 2023', '2023-03-15', NULL);
 
 -- Exo 2.3
 -- Créez une relation entre un student et un tag :
@@ -42,11 +50,17 @@ WHERE id = 6
 -- Exo 2.4
 -- Mettez à jour le tag `HTML` :
 -- description : Le HTML est un language de structuration de données.
+UPDATE tag
+SET description = 'Le HTML est un language de structuration de données'
+WHERE id = 1;
 
 -- Exo 2.5
 -- Mettez à jour le tag `Bash` :
 -- nom : Terminal Bash
 -- description : Le terminal Bash est l'interface en ligne de commande par défaut sous Linux.
+UPDATE tag
+SET name = 'Terminal Bash', description = 'Le terminal Bash est l''interface en ligne de commande par défaut sous Linux'
+WHERE id = 10;
 
 -- Exo 2.6
 -- Supprimez la relation entre un student et un tag :
