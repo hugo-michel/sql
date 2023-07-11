3b4d8b867b4571b5b3f83c1e76ca837149752518
hugo-michel <orayito62@gmail.com>
2023-07-11 16:26:34 +0200

    exo sql 05

--- exercice-05.sql
+++ exercice-05.sql
@@ -3,13 +3,27 @@
 
 -- Exo 5.1
 -- Listez tous les students avec leurs tags
-
+SELECT student.firstname, student.lastname, tag.name
+FROM `student_tag`
+INNER JOIN student ON student_tag.student_id = student.id
+INNER JOIN tag ON student_tag.tag_id = tag.id;
 -- Exo 5.2
 -- Listez tous les tags avec leurs students
-
+SELECT tag.name, student.firstname, student.lastname
+FROM `student_tag`
+INNER JOIN tag ON student_tag.tag_id = tag.id
+INNER JOIN student ON student_tag.student_id = student.id;
 -- Exo 5.3
 -- Listez le student dont l'id est `2` avec ses tags
-
+SELECT student.firstname, student.lastname, tag.name
+FROM `student_tag`
+INNER JOIN student ON student_tag.student_id = student.id
+INNER JOIN tag ON student_tag.tag_id = tag.id
+WHERE student_id = 2;
 -- Exo 5.4
 -- Listez le tag dont l'id est `2` avec ses students
-
+SELECT tag.name, student.firstname, student.lastname
+FROM `student_tag`
+INNER JOIN tag ON student_tag.tag_id = tag.id
+INNER JOIN student ON student_tag.student_id = student.id
+WHERE tag_id = 2;
