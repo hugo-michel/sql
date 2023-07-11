46545ba0a6231a584ca3435cd7a2f258bca0b297
hugo-michel <orayito62@gmail.com>
2023-07-11 15:00:12 +0200

    exo sql 04

--- exercice-04.sql
+++ exercice-04.sql
@@ -4,14 +4,28 @@
 -- Exo 4.1
 -- Listez tous les students avec leurs projects
 -- Note : les students sans projets doivent aussi apparaître dans les résultats
-
+SELECT student.firstname, student.lastname, project.name
+FROM `student`
+LEFT OUTER JOIN project
+ON student.project_id = project.id;
 -- Exo 4.2
 -- Listez le student dont l'id est `2` avec son project
-
+SELECT student.firstname, student.lastname, project.name
+FROM `student`
+LEFT OUTER JOIN project
+ON student.project_id = project.id
+WHERE student.id = 2;
 -- Exo 4.3
 -- Listez tous les projects avec leurs students
 -- Note : les projets sans studets ne doivent pas apparaître dans les résultats
-
+SELECT project.name, student.firstname, student.lastname
+FROM project
+INNER JOIN student
+ON project.id = student.project_id;
 -- Exo 4.4
 -- Listez le project dont l'id est `3` avec ses students
-
+SELECT project.name, student.firstname, student.lastname
+FROM project
+INNER JOIN student
+ON project.id = student.project_id
+WHERE project.id = 3;
